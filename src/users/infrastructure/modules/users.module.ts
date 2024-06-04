2227bce3e414aeb86b305f9199da2da2a9615c53
SharpBlade <yepezedwin760@gmail.com>
2024-06-04 18:19:34 -0500

    actualización de archivos para entidades

--- src/users/infrastructure/modules/users.module.ts
+++ src/users/infrastructure/modules/users.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../../domain/entities/user.entity';
 import { UsersController } from '../controllers/users.controller';
 import { UsersService } from '../../domain/services/users.service';
+import { Users } from 'src/common/entities/Users';
+import { RoleUser } from 'src/common/entities/RoleUser';
+import { Role } from 'src/common/entities/Role';
+import { Menu } from 'src/common/entities/Menu';
+import { RoleMenu } from 'src/common/entities/RoleMenu';
+import { Permission } from 'src/common/entities/Permission';
+import { MenuPermission } from 'src/common/entities/MenuPermission';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Users,
+      RoleUser,
+      Role,
+      Menu,
+      RoleMenu,
+      Permission,
+      MenuPermission,
+    ]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
