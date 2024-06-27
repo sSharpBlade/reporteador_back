50a648ade9a116d6528fd482cf91cadaff0ede13
Oscar Joel Ramírez Manzano <oramirez0004@uta.edu.ec>
2024-06-26 22:48:28 -0500

    servicio sql

--- src/sql/domain/sql.service.ts
+++ src/sql/domain/sql.service.ts
@@ -1,8 +1,16 @@
-/*
-https://docs.nestjs.com/providers#services
-*/
-
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Query } from '../../common/entities/Query.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class SqlService {}
+export class SqlService {
+    constructor (
+        @InjectRepository(Query)
+        private readonly queryRepository:Repository<Query>
+    ){}
+
+    async create(sentence:string) {
+        return await this.queryRepository.save({sentence});
+ }
+}
