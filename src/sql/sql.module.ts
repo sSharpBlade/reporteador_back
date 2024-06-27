1d68c4194e009cd8291f5d2cfe4104e9a0de5f57
Oscar Joel Ramírez Manzano <oramirez0004@uta.edu.ec>
2024-06-26 22:51:41 -0500

    modulo de sql

--- src/sql/sql.module.ts
+++ src/sql/sql.module.ts
@@ -5,11 +5,13 @@ import { DatabaseService } from '../database/domain/database.service';
 import { SqlExecutorService } from './domain/sqlexecutor.service';
 import { SqlController } from './infrastructure/sql.controller';
 import { AuthModule } from '../auth/auth.module';
+import { SqlService } from './domain/sql.service';
+import { Query } from '../common/entities/Query.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DatabaseConnection]), AuthModule],
-  providers: [DatabaseService, SqlExecutorService],
+  imports: [TypeOrmModule.forFeature([DatabaseConnection, Query]), AuthModule],
+  providers: [DatabaseService, SqlExecutorService, SqlService],
   controllers: [SqlController],
   exports: [SqlExecutorService],
 })
-export class SqlModule {}
+export class SqlModule {}
